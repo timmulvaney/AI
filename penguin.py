bef32bf7d9f3229e297f2f53d29a1606165be77d
timmulvaney <146193831+timmulvaney@users.noreply.github.com>
2024-02-21 21:53:42 +0000

    Add files via upload

--- penguin.py
+++ penguin.py
@@ -21,45 +21,46 @@ df.dropna(inplace=True)
 
 # pivot on the islands where they live
 df['island'].value_counts().plot(kind='bar',color=['#d5e0fe','#656371','#ff7369'])
+plt.show()
 
 # now on the kind
 sns.set_style('white')
 df['species'].value_counts().plot(kind='barh',color=['#6baddf','#01193f','#d2b486'])
+plt.show()
 
 # bill length, island and species
 #  - Adelie lives in all the three islands.
 #  - Gentoo resides only in Biscoe.
 #  - Gentoo and Chinstrap have lengthier bills compared to Adelie.
 sns.swarmplot(x=df.island,y=df.bill_length_mm,hue=df.species)
+plt.show()
 
 # mass, species and sex
 sns.set_style('dark')
 sns.boxplot(x=df.species,y=df.body_mass_g,hue=df.sex)
+plt.show()
 
 # bill_length and bill_depth and species
 #  Clearly three groups of species can be identified.
 #  Each of the species bill_length and bill_depth fall in a certain range.
-
-
 sns.set_style('dark')
 sns.scatterplot(x=df.bill_length_mm,y=df.bill_depth_mm,hue=df.species)
+plt.show()
 
 # correlation of features
 correlation_matrix=df.corr()
 correlation_matrix
+plt.show()
 
 # heatmap of features
 # Flipper length and body_mass are strongly dependent with corelation value of 0.87.
 # In other words penguins with longer flips, generally weigh more
 sns.set_style('dark')
 sns.heatmap(correlation_matrix,annot=True,linecolor='white',linewidths=5,cmap="YlGnBu")
+plt.show()
 
 # Multiple Bivariate distribution
 # Linear relationships can be seen in most of the plots 
 sns.set_style(style='white')
 sns.pairplot(data=df,hue='species',palette=['#6baddf','#01193f','#d2b486'])
-
-
-
-
-
+plt.show()
